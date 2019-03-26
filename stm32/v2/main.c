c0e7b096e24bca34ffcdcffcab2c3d0c2eeeedbb
skywoodsz <36660984+skywoodsz@users.noreply.github.com>
2019-03-26 20:31:57 +0800

    Add files via upload

---
+++ stm32/v2/main.c
@@ -0,0 +1,50 @@
+#include "stm32f10x.h"
+#include "SysTick.h"
+#include "system.h"
+#include "data.h"
+
+#include "led.h"
+#include "key.h"
+#include "usart.h"
+#include "tftlcd.h"
+extern u8 Uart1_Buffer[Max_BUFF_Len];
+extern u8 send_flag;
+int char2int(char* str);
+int main()
+{
+	int pc_data;
+	u8 getchar[20]= "1"; 
+	SysTick_Init(72);
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);//设置中断分组
+	USART1_Init(9600);//初始化串口
+	TFTLCD_Init();			//LCD初始化
+	FRONT_COLOR=BLACK;
+	LCD_ShowString(10,10,tftlcd_data.width,tftlcd_data.height,24,getchar);
+	while(1)
+	{
+		if(send_flag == 1)
+		{
+			send_flag = 0;
+			//Send_data(Uart1_Buffer);
+			pc_data = char2int(Uart1_Buffer);
+			//USART_SendData(USART1,pc_data);
+			//LCD_ShowxNum(10,30, pc_data,1,24,0);
+			if(Uart1_Buffer[0]== 0x05)
+			{
+				LCD_ShowxNum(10,30,5,1,24,0);
+			}
+		}		
+	}
+} 
+int char2int(char* str)
+{
+	u8 i;
+	int num;
+	for(i = 0,num = 0;str[i]!='\0';i++)
+	{
+		num = num*10 + (str[i] - '0');
+	}
+	//USART_SendData(USART1,num);
+	return num;
+}
+
